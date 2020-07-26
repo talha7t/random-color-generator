1fca3be1425278fc13797121bfd976a37256a080
Talha <48598366+talha7t@users.noreply.github.com>
2020-07-26 15:21:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var container = document.getElementById("container");
-var title = document.querySelector(".title");
-var generateColor = document.querySelector(".generate-color");
+var containers = Array.from(document.querySelectorAll(".color-container"));
+var titles = Array.from(document.querySelectorAll(".title"));
+var generateColor = Array.from(document.querySelectorAll(".generate-color"));
+const colorsContainer = document.getElementById("container");
 
-generateColor.addEventListener("click", changeColor, false);
+// generating random colors for the first time
+containers.forEach((container, index) => {
+  changeColor(index);
+});
 
-function changeColor() {
-  var color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+//listening for any clicks on colorsContainer
+colorsContainer.addEventListener("click", (e) => {
+  generateColor.forEach((generator, index) => {
+    if (generateColor[index] == e.target) {
+      //if click is on generate button, change color of respective color-container
+      changeColor(index);
+    }
+  });
+});
 
-  container.style.backgroundColor = color;
-  title.innerHTML = color;
+function changeColor(index) {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  containers[index].style.backgroundColor = color;
+  titles[index].textContent = color;
 }
