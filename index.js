4c8a2b8fa79b8ee16493b6ca073d38a15e4de042
Talha <48598366+talha7t@users.noreply.github.com>
2020-07-30 15:05:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+/*---------------VARIABLE INITIALIZATION---------------------- */
+
 var containers = Array.from(document.querySelectorAll(".color-container"));
 var titles = Array.from(document.querySelectorAll(".title"));
 var generateColor = Array.from(document.querySelectorAll(".generate-color"));
 const colorsContainer = document.getElementById("container");
+var copyContent = Array.from(document.querySelectorAll(".text-copy img"));
 
 // generating random colors for the first time
 containers.forEach((container, index) => {
   changeColor(index);
 });
 
+/*-----------------EVENT LISTENERS--------------- */
+
 //listening for any clicks on colorsContainer
-colorsContainer.addEventListener("click", (e) => {
-  generateColor.forEach((generator, index) => {
-    if (generateColor[index] == e.target) {
-      //if click is on generate button, change color of respective color-container
-      changeColor(index);
-    }
-  });
-});
+colorsContainer.addEventListener("click", handleContainerEvents);
+
+/*--------------FUNCTIONS---------------- */
 
 function changeColor(index) {
+  //function to generate random color and change background of container
   var letters = "0123456789ABCDEF";
   var color = "#";
   for (var i = 0; i < 6; i++) {
@@ -27,4 +28,31 @@ function changeColor(index) {
 
   containers[index].style.backgroundColor = color;
   titles[index].textContent = color;
+}
+
+function handleContainerEvents(e) {
+  //function to handle events on color-containers
+
+  generateColor.forEach((generator, index) => {
+    if (generator == e.target) {
+      //if click is on generate button, change color of respective color-container
+      changeColor(index);
+    }
+  });
+  copyContent.forEach((content, index) => {
+    // if click is on copy content icon, copy text of color in the color-container
+    if (e.target == content) {
+      copyText(index);
+    }
+  });
+}
+
+function copyText(index) {
+  //function to copy text to clipboard
+  const selection = window.getSelection(); // save the selection
+  const range = document.createRange(); // create range
+  range.selectNode(titles[index]);
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand("copy");
 }
